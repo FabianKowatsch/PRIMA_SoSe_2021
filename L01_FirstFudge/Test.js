db4b9efb45e5566ad9bb3a44f3aec70da8c2a8a3
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2021-03-23 18:38:03 +0100

    first fudge

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -2,7 +2,21 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var f = FudgeCore;
-    let node = new f.Node("test");
-    console.log(node);
+    window.addEventListener("load", hndlLoad);
+    function hndlLoad(_event) {
+        const canvas = document.querySelector("#viewport");
+        let node = new f.Node("test");
+        let mesh = new f.MeshQuad("Quad");
+        let material = new f.Material("Mat", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
+        node.addComponent(new f.ComponentMesh(mesh));
+        node.addComponent(new f.ComponentMaterial(material));
+        let cmpCamera = new f.ComponentCamera();
+        cmpCamera.mtxPivot.translateZ(3);
+        cmpCamera.mtxPivot.rotateY(170);
+        let viewport = new f.Viewport();
+        console.log(cmpCamera);
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+    }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,6 +1,26 @@
 namespace L01_FirstFudge {
   import f = FudgeCore;
 
-  let node: f.Node = new f.Node("test");
-  console.log(node);
+  window.addEventListener("load", hndlLoad);
+
+  function hndlLoad(_event: Event): void {
+    const canvas: HTMLCanvasElement = document.querySelector("#viewport");
+    let node: f.Node = new f.Node("test");
+    let mesh: f.MeshQuad = new f.MeshQuad("Quad");
+    let material: f.Material = new f.Material(
+      "Mat",
+      f.ShaderUniColor,
+      new f.CoatColored(new f.Color(1, 1, 1, 1))
+    );
+    node.addComponent(new f.ComponentMesh(mesh));
+    node.addComponent(new f.ComponentMaterial(material));
+
+    let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
+    cmpCamera.mtxPivot.translateZ(3);
+    cmpCamera.mtxPivot.rotateY(170);
+    let viewport: f.Viewport = new f.Viewport();
+    console.log(cmpCamera);
+    viewport.initialize("Viewport", node, cmpCamera, canvas);
+    viewport.draw();
+  }
 }
