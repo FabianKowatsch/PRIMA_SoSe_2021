cb51987657f6c3693ff8ea9f12c7c486db5356f8
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2021-03-25 18:01:10 +0100

    added game loop

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -2,20 +2,31 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var f = FudgeCore;
+    const frameRate = 60;
+    const rotSpeed = 90;
+    let node = new f.Node("test");
+    let viewport = new f.Viewport();
     window.addEventListener("load", hndlLoad);
     function hndlLoad(_event) {
         const canvas = document.querySelector("#viewport");
-        let node = new f.Node("test");
-        let mesh = new f.MeshQuad("Quad");
-        let material = new f.Material("Mat", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
+        let mesh = new f.MeshCube("Cube");
+        let material = new f.Material("Mat", f.ShaderUniColor, new f.CoatColored(new f.Color(0.6, 1, 0, 1)));
+        node.addComponent(new f.ComponentTransform());
         node.addComponent(new f.ComponentMesh(mesh));
         node.addComponent(new f.ComponentMaterial(material));
         let cmpCamera = new f.ComponentCamera();
         cmpCamera.mtxPivot.translateZ(3);
-        cmpCamera.mtxPivot.rotateY(170);
-        let viewport = new f.Viewport();
+        cmpCamera.mtxPivot.rotateY(180);
         console.log(cmpCamera);
         viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+        f.Loop.start(f.LOOP_MODE.TIME_REAL, frameRate);
+        f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+    }
+    function update(_event) {
+        node
+            .getComponent(f.ComponentTransform)
+            .mtxLocal.rotateY((rotSpeed * f.Loop.timeFrameReal) / 1000);
         viewport.draw();
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,26 +1,40 @@
 namespace L01_FirstFudge {
   import f = FudgeCore;
 
+  const frameRate: number = 60;
+  const rotSpeed: number = 90;
+
+  let node: f.Node = new f.Node("test");
+  let viewport: f.Viewport = new f.Viewport();
+
   window.addEventListener("load", hndlLoad);
 
   function hndlLoad(_event: Event): void {
     const canvas: HTMLCanvasElement = document.querySelector("#viewport");
-    let node: f.Node = new f.Node("test");
-    let mesh: f.MeshQuad = new f.MeshQuad("Quad");
+    let mesh: f.MeshCube = new f.MeshCube("Cube");
     let material: f.Material = new f.Material(
       "Mat",
       f.ShaderUniColor,
-      new f.CoatColored(new f.Color(1, 1, 1, 1))
+      new f.CoatColored(new f.Color(0.6, 1, 0, 1))
     );
+    node.addComponent(new f.ComponentTransform());
     node.addComponent(new f.ComponentMesh(mesh));
     node.addComponent(new f.ComponentMaterial(material));
 
     let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
     cmpCamera.mtxPivot.translateZ(3);
-    cmpCamera.mtxPivot.rotateY(170);
-    let viewport: f.Viewport = new f.Viewport();
+    cmpCamera.mtxPivot.rotateY(180);
     console.log(cmpCamera);
     viewport.initialize("Viewport", node, cmpCamera, canvas);
+    viewport.draw();
+
+    f.Loop.start(f.LOOP_MODE.TIME_REAL, frameRate);
+    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
+  }
+  function update(_event: Event): void {
+    node
+      .getComponent(f.ComponentTransform)
+      .mtxLocal.rotateY((rotSpeed * f.Loop.timeFrameReal) / 1000);
     viewport.draw();
   }
 }
